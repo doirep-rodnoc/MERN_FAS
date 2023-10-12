3142b5fb8db9623194d08062d6fd42826d89c53e
doirep_rodnoc <51051070+doirep-rodnoc@users.noreply.github.com>
2023-10-12 22:27:15 +0900

    login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
+
 // ユーザの登録
 router.post("/register", async (req, res) => {
     try {
@@ -27,10 +28,14 @@ router.post("/login", async (req, res) => {
         const isPwdMatch = await user.checkPwd(password);
         if (!isPwdMatch) return res.status(400).send("Invalid Password.");
 
+        const token = jwt.sign({ email }, process.env.JWT_SECRET_KEY, { expiresIn: process.env.JWT_EXPIRES_IN, });
+
+        res.cookie("token", token, { httpOnly: true });
         return res.status(200).json(user);
     } catch (error) {
+        console.log(error);
         return res.status(500).json(error);
     }
-})
+});
 
 module.exports = router;
